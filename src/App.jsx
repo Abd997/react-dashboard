ca67f7689b52693bfeeb289ed4c36854cd250e7f
Ijaz Ul Rehman <ijazulrehmannajeeb@gmail.com>
2024-03-27 03:27:14 +0500

    chore: added intital work

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import './App.css'
+import { ColorModeContext, useMode } from './theme'
+import Topbar from './components/topbar'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  const [theme, colorMode] = useMode()
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+}
+
+export default App
